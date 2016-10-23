41a08fa160771aded07694735dac50797219ddd7
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2016-10-22 22:13:29 -0500

    upodated version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='scrapeo',
-        version='0.1.1.b2',
+        version='0.1.1.rc1',
         author='Paul Morris',
         author_email='wheresmyjetpack03@gmail.com',
         #url='http://www.scrapeo.org',
