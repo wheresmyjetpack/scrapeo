12dc5755f92911752c738fad68aabf0a17bdb75b
Paul <wheresmyjetpack03@gmail.com>
2016-10-26 17:03:13 -0500

    place utils TODO in a docstring

--- scrapeo/utils/web_scraper.py
+++ scrapeo/utils/web_scraper.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
-# TODO This needs to be a set of module-level functions, and is not
-# really a web scraper, but just a set of high level functions for
-# making HTTP requests.
+"""Utils for the Scrapeo package
+
+.. todo:: This needs to be a set of module-level functions, and is not
+    really a web scraper, but just a set of high level functions for
+    making HTTP requests.
+"""
 
 import requests
 
 class WebScraper(object):
-    # TODO This should just be a set of top-level module functions, not
-    # a class
 
     def __init__(self, http_client=None):
         self.http_client = http_client or self.__default_http_client()
